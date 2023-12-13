5e74c67418d1c8189b320b668dcee20e991a283b
behzod234963 <behzod234963@internet.ru>
2023-12-13 17:30:12 +0500

    jetpack compose

--- app/src/main/java/com/coder/behzod/jetpackcomposepractice/ListItem.kt
+++ app/src/main/java/com/coder/behzod/jetpackcomposepractice/ListItem.kt
@@ -1,6 +1,9 @@
 package com.coder.behzod.jetpackcomposepractice
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,13 +15,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,29 +37,66 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun ListItem(name:String,prof:String){
-
-    Column {
-        Card(modifier = Modifier
+fun ListItem(name: String, prof: String) {
+    var count = remember {
+        mutableIntStateOf(0)
+    }
+    var color = remember {
+        mutableStateOf(Color.Red)
+    }
+    Card(
+        modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp),
-            shape = RoundedCornerShape(15.dp),
-            elevation = CardDefaults.cardElevation(5.dp)
-        ){
-            Column {
-                Row {
-                    Image(painter = painterResource(id = R.drawable.ic_launcher_background)
-                        , contentDescription = "fgnhjm",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .size(64.dp)
-                            .clip(CircleShape))
-                    Column(modifier = Modifier
-                        .padding(start = 10.dp,top = 10.dp)
-                        ,verticalArrangement = Arrangement.Center) {
-                        Text(text = name, fontSize = 20.sp)
-                        Text(text = prof)
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "fgnhjm",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .size(64.dp)
+                    .clip(CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp, top = 10.dp), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = name, fontSize = 20.sp)
+                Text(text = prof)
+            }
+            Column (verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                Card (elevation = CardDefaults.cardElevation(10.dp)
+                    ,shape = RoundedCornerShape(10.dp)
+                ){
+                    Column(
+                        Modifier
+                            .clickable {
+                                ++count.value
+                                when(count.value){
+                                    10->{
+                                        color.value = Color.Blue
+                                    }
+                                    20->{
+                                        color.value = Color.Black
+                                    }
+                                    30->{
+                                        color.value = Color.Magenta
+                                    }
+                                }
+                            }.size(
+                                width = 120.dp,
+                                height = 60.dp
+                            )
+                            .background(color.value),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(text = count.value.toString()
+                            , modifier = Modifier)
                     }
                 }
             }
--- app/src/main/java/com/coder/behzod/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/coder/behzod/jetpackcomposepractice/MainActivity.kt
@@ -8,15 +8,21 @@ import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -24,19 +30,33 @@ import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.tooling.preview.Wallpapers
 import kotlin.reflect.KClass
 
-const val NAME = "behzod coder"
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
-             ListItem(name = "Behzod", prof = "Coder")
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem(name = "jvhvgggjjh", prof = "jhvhjvgvvv")
+                ListItem(name = "cngfm", prof = "Coder")
+                ListItem(name = "cngfhmhj,", prof = "Coder")
+                ListItem(name = "dffghnhmj,", prof = "Coder")
+                ListItem(name = "dffdnhg", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+                ListItem(name = "xfngh", prof = "Coder")
+            }
         }
     }
 }
@@ -46,8 +66,10 @@ fun MainActivityUI(){
     Column(modifier = Modifier
         .fillMaxSize()
         .background(Color.Red),
-        verticalArrangement = Arrangement.Center) {
-        Text(text = "Behzod")
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Behzod", modifier = Modifier.clickable {
+        })
         Text(text = "Behzod")
         Text(text = "Behzod")
     }
